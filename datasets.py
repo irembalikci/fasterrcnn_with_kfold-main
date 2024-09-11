5ba0fec978fdb3470d6ab97ef0befa204fbe64ea
İrem Balıkcı <91717168+irembalikci@users.noreply.github.com>
2024-09-11 15:46:40 +0300

    modified getitem

--- datasets.py
+++ datasets.py
@@ -311,14 +311,14 @@ class CustomDataset(Dataset):
         return result_image, torch.tensor(result_boxes), \
             torch.tensor(np.array(final_classes)), area, iscrowd, dims
 
-    def __getitem__(self, idx, index):
+    def __getitem__(self, idx):
         # Check if index is valid
-        if index >= len(self.all_images):
-            print(f"Index {index} is out of range for dataset size {len(self.all_images)}")
-            raise IndexError(f"Index {index} is out of range")
+        if idx >= len(self.all_images):
+            print(f"Index {idx} is out of range for dataset size {len(self.all_images)}")
+            raise IndexError(f"Index {idx} is out of range")
         
         # Proceed with loading the image and labels
-        labels, area, iscrowd, dims = self.load_image_and_labels(index)
+        labels, area, iscrowd, dims = self.load_image_and_labels(idx)
         if not self.train: # No mosaic during validation.
             image, image_resized, orig_boxes, boxes, \
                 labels, area, iscrowd, dims = self.load_image_and_labels(
